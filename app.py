cb23ba9b8dd5857c82bde63a1780c0e3b8136584
SoulReaper <81975481+sourcersupreme@users.noreply.github.com>
2026-03-27 17:08:35 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from optimized_model import predict_fast
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "✅ Phishing Detection API Running"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json
+    text = data.get("text", "")
+    
+    result = predict_fast(text)
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
