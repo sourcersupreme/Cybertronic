4a75f41709eff35eb89bbe00871a5687d7a58b0e
SoulReaper <81975481+sourcersupreme@users.noreply.github.com>
2026-03-28 12:37:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,8 @@ def predict():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+    
+    from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # 🔥 THIS LINE IS REQUIRED
