63680dd3bf5556b3007b174043a676b4cbba4257
SoulReaper <81975481+sourcersupreme@users.noreply.github.com>
2026-03-28 13:01:43 +0530

    Update content.js

--- extension/content.js
+++ extension/content.js
@@ -51,7 +51,7 @@ function scanEmail() {
     console.log("Extracted text:", text);
 
     if (text.length > 0) {
-        fetch("http://localhost:5000/predict", {
+        fetch("http://127.0.0.1:5000/predict", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ text: text })
